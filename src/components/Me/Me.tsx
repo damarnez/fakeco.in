c83a3f87efa79716ef29f854e80d9e52110a80cb
damarnez <daniel@raise.it>
2020-04-07 18:06:09 +0200

    Change tip address

--- src/components/Me/Me.tsx
+++ src/components/Me/Me.tsx
@@ -8,7 +8,7 @@ import GithubIco from "../../icons/iconmonstr-github-3.svg";
 
 import EthIco from "../../icons/eth.svg";
 import Grid from "@material-ui/core/Grid";
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     textAlign: "left",
     paddingTop: "150px",
@@ -29,50 +29,50 @@ const useStyles = makeStyles(theme => ({
       top: "-20%",
 
       backgroundImage: "url(./images/cta-illustration.svg)",
-      zIndex: 0
-    }
+      zIndex: 0,
+    },
   },
   card: {
     background: "#4A61DD",
     borderRadius: 0,
     padding: "64px 32px",
-    display: "block"
+    display: "block",
   },
   avatar: {
     width: "200px",
-    height: "200px"
+    height: "200px",
   },
   buttons: {
     float: "right",
-    height: "100%"
+    height: "100%",
   },
   items: {
     zIndex: 101,
     padding: "0px",
-    margin: "0px"
+    margin: "0px",
   },
   title: {
     fontSize: "32px",
     margin: "0px",
     paddingTop: "4px",
-    color: "#fff"
+    color: "#fff",
   },
   subtitle: { fontSize: "20px" },
   span: {
     fontSize: "20px",
     position: "relative",
-    top: "10px"
+    top: "10px",
   },
   grid: {
     display: "flex",
     marginLeft: "50px",
     [theme.breakpoints.down("md")]: {
-      marginLeft: "0px"
-    }
+      marginLeft: "0px",
+    },
   },
   icons: {
     color: "#8A94A7",
-    width: "24px"
+    width: "24px",
   },
   links: {
     color: "#DDE2F4",
@@ -83,9 +83,9 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.down("md")]: {
       maxWidth: "230px",
       overflow: "hidden",
-      display: "inline-flex"
-    }
-  }
+      display: "inline-flex",
+    },
+  },
 }));
 
 const Me = () => {
@@ -138,9 +138,7 @@ const Me = () => {
             </p>
             <p>
               <img src={EthIco} className={classes.icons} />
-              <span className={classes.links}>
-                Tip me at : 0xd8031f2CB879C7A087F11526Fab9b1B315575EF6
-              </span>
+              <span className={classes.links}>Tip me at : damarnez.eth</span>
             </p>
           </Grid>
         </Grid>
