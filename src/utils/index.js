e9634d088cb14b749eabd120057c505318cc4c01
Daniel Martin <daniel@herotoken.io>
2020-04-16 12:27:53 +0200

    fix error big number

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,4 +3,4 @@ import BigNumber from "bignumber.js";
 export const toDecimal = (etherAmount, factor = 18) =>
   new BigNumber(etherAmount)
     .multipliedBy(new BigNumber("10").pow(new BigNumber(factor.toString())))
-    .toString();
+    .toString(10);
